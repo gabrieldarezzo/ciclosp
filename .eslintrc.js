754b13e1adc6d1c37294bff56c69797a16f88380
Gabriel <gabrieldarezzo@yahoo.com.br>
2017-10-06 10:08:58 -0300

    package.json, index.html and tests start files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+  ],
+   "env": {
+      "browser": true,
+      "mocha": true,
+      "es6": true
+  },
+  "rules": {
+    "no-param-reassign": 0,
+    "linebreak-style": 0
+  }
+};
